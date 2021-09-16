470f630c33c7f820986d2328eec55559cbb1c3e7
root <moses@udacity.com>
2021-09-16 08:54:25 +0000

    Added method to wait fpr green

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -84,6 +84,10 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
     lck.lock();
+    
+    if(!trafficLightIsGreen()){
+        _trafficLight.waitForGreen();
+    }
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
